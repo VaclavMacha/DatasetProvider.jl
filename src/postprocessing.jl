6b0bfe52cf799ad26cc54658f9dd59d82d191ad4
VaclavMacha <vaclav.macha92@gmail.com>
2021-06-14 22:16:22 +0200

    Fixed partition for regression data

--- src/postprocessing.jl
+++ src/postprocessing.jl
@@ -1,58 +1,14 @@
-# partition by classes
-partition_size(targets, at) = partition_size(targets, [at...,])
-
-function partition_size(targets, at::AbstractVector)
-    n = StatsBase.counts(targets)
-    ns = round.(Int, n .* at')
-    return [ns n .- sum(ns, dims = 2)]
-end
-
-function targets_map(targets)
-    classes = sort(unique(targets))
-    mapping = Dict(tuple.(classes, 1:length(classes)))
-    return y -> mapping[y]
-end
-
-targets_map(::BitArray) = y -> y ? 2 : 1
-
-function partition_class(targets, at)
-    ymap = targets_map(targets)
-    ns = partition_size(ymap.(targets), at)
-
-    parts = zeros.(Int, sum(ns; dims = 1))
-    class_part = ones(Int, size(ns, 1))
-    class_count = zero(class_part)
-    pos_part = ones(Int, size(ns, 2))
-
-    for (ind, y) in pairs(targets)
-        class = ymap(y)
-
-        count_max = ns[class, class_part[class]]
-        if class_count[class] == count_max
-            class_count[class] = 1
-            class_part[class] += 1
-        else
-            class_count[class] += 1
-        end
-
-        i = class_part[class]
-        parts[i][pos_part[i]] = ind
-        pos_part[i] += 1
-    end
-    return Tuple(parts)
-end
-
 # data splitting
-function data_split(data::Tuple, obsdim, at)
+function data_split(d::Dataset, data::Tuple, obsdim, at)
     x, y = data
-    return map(partition_class(y, at)) do inds
+    return map(partition(d, y, at)) do inds
         return selectdim(x, obsdim, inds), selectdim(y, 1, inds)
     end
 end
 
-function data_split(data::AbstractDataFrame, obsdim, at)
+function data_split(d::Dataset, data::AbstractDataFrame, obsdim, at)
     y = data.targets |> Vector
-    return map(partition_class(y, at)) do inds
+    return map(partition(d, y, at)) do inds
         return data[inds, :]
     end
 end
