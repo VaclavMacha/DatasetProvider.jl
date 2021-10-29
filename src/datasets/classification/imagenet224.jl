4ba0b4f4d57fe6464110f8b8b966e590c64e74d9
machava2 <machava2@login1.rci.cvut.cz>
2021-10-29 17:08:21 +0200

    Added ImageNet224MMAP and ImageNet224

--- src/datasets/classification/imagenet224.jl
+++ src/datasets/classification/imagenet224.jl
@@ -1,4 +1,6 @@
-struct ImageNet224 <: Name end
+abstract type AbstractImageNet224 <: Name end
+struct ImageNet224MMAP <: AbstractImageNet224 end
+struct ImageNet224 <: AbstractImageNet224 end
 
 const IMAGENET_PATH = Ref{String}(joinpath(DATADIR, "ImageNet224.h5"))
 const IMAGENET_FILE = Ref{HDF5.File}(HDF5.File(-1, "nnn"))
@@ -21,25 +23,25 @@ end
 close_imagenet() = close(IMAGENET_FILE[])
 
 # download options
-task(::Type{ImageNet224}) = MultiClass
-format(::Type{ImageNet224}) = ColorImages
-source(::Type{ImageNet224}) = "https://image-net.org/"
-version(::Type{ImageNet224}) = "ILSVRC2012"
-make_datadep(::Type{ImageNet224}) = nothing
+task(::Type{<:AbstractImageNet224}) = MultiClass
+format(::Type{<:AbstractImageNet224}) = ColorImages
+source(::Type{<:AbstractImageNet224}) = "https://image-net.org/"
+version(::Type{<:AbstractImageNet224}) = "ILSVRC2012"
+make_datadep(::Type{<:AbstractImageNet224}) = nothing
 
-nclasses(::Type{ImageNet224}) = 1000
-classes(::Type{ImageNet224}) = collect(1:1000)
-nattributes(::Type{ImageNet224}) = (224, 224, 3)
-ninstances(::Type{ImageNet224}) = (1281167, 0, 50000)
+nclasses(::Type{<:AbstractImageNet224}) = 1000
+classes(::Type{<:AbstractImageNet224}) = collect(1:1000)
+nattributes(::Type{<:AbstractImageNet224}) = (224, 224, 3)
+ninstances(::Type{<:AbstractImageNet224}) = (1281167, 0, 50000)
 
 # dataset description
-name(::Type{ImageNet224}) = "ImageNet Large Scale Visual Recognition Challenge 2012"
+name(::Type{<:AbstractImageNet224}) = "ImageNet Large Scale Visual Recognition Challenge 2012"
 
-function author(::Type{ImageNet224})
+function author(::Type{<:AbstractImageNet224})
     return ["Olga Russakovsky", "Jia Deng", "Hao Su", "Jonathan Krause", "Sanjeev Satheesh", "Sean Ma", "Zhiheng Huang", "Andrej Karpathy", "Aditya Khosla", "Michael Bernstein", "Alexander C. Berg", "Li Fei-Fei"]
 end
 
-function citation(::Type{ImageNet224})
+function citation(::Type{<:AbstractImageNet224})
     return """
     @article{ILSVRC15,
         Author = {Olga Russakovsky and Jia Deng and Hao Su and Jonathan Krause and Sanjeev Satheesh and Sean Ma and Zhiheng Huang and Andrej Karpathy and Aditya Khosla and Michael Bernstein and Alexander C. Berg and Li Fei-Fei},
@@ -90,13 +92,18 @@ function Base.selectdim(A::HDF5Array{T,N}, d::Int, inds) where {T, N}
     return HDF5Array(A.data, A.inds[inds])
 end
 
-function loadraw(N::Type{ImageNet224}, type)
+function loadraw(N::Type{<:AbstractImageNet224}, type)
     hassubset(N, type)
     split = type == :test ? "val" : "train"
 
     fid = open_imagenet()
-    data = HDF5.readmmap(fid["$(split)_data"])
+    if N <: ImageNet224MMAP
+        data = HDF5.readmmap(fid["$(split)_data"]) |> HDF5Array
+    else
+        @warn "due to the size of the ImageNet dataset, it may take several minutes to load data into memory"
+        data = HDF5.read(fid["$(split)_data"])
+    end
     targets = fid["$(split)_targets"][:]
     close(fid)
-    return HDF5Array(data), targets
+    return data, targets
 end
